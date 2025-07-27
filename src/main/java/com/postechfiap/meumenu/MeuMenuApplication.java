3a462abae55554edce89305e3e665276ea052cb4
franciscoabsl <franciscosouzalima@gmail.com>
2025-07-27 14:52:03 -0300

    configuracoes iniciais

---
+++ src/main/java/com/postechfiap/meumenu/MeuMenuApplication.java
@@ -0,0 +1,13 @@
+package com.postechfiap.meumenu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeuMenuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeuMenuApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/postechfiap/meumenu/MeumenuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.postechfiap.meumenu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeuMenuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
